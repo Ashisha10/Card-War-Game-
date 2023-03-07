43a54a1071d21f506c5a492419acee543fa5185c
Ashisha Tulsian <ashishatulsian10@gmail.com>
2023-03-07 21:20:25 +0530

    Initial Commit

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Ashisha Tulsian on 07/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Ashisha Tulsian on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
